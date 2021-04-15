152e51bc0bd815ddd57c2bec128a85dbd9559cbd
kr0ck <hebertcampos@hotmail.com.br>
2021-04-14 21:42:15 -0300

    Construindo layout da screen vigens

--- lib/Screens/Viagens_Screen.dart
+++ lib/Screens/Viagens_Screen.dart
@@ -13,9 +13,44 @@ class _ViagensScreenState extends State<ViagensScreen> {
       appBar: AppBar(
         title: Text('Viagens'),
       ),
-      body: Center(
-        child: Text('Ainda não existem dados!'),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            height: 100,
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                'R\$ valor',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.all(10),
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 2,
+                  itemBuilder: (_, index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text('Loja'),
+                        subtitle:
+                            Text('01/01/2021', style: TextStyle(fontSize: 10)),
+                        trailing: Text('R\$ 00,00'),
+                      ),
+                    );
+                  })),
+        ],
       ),
+      // Center(
+      //   child: Text('Ainda não existem dados!'),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).push(
